335b9773fa55bea134e3d8f84c51de229aa388b3
Regner Blok-Andersen <shadowdf@gmail.com>
2015-05-05 16:49:35 +0000

    Began flushing out the EVE Trivia web site with some of the ideas I have for organizing not just the web site but the whole EVE Trivia project.

--- js/evetrivia.js
+++ js/evetrivia.js
@@ -1,14 +1,14 @@
 $.getJSON('https://erebus-regner.c9.io/questions/random/', function(data){
-    var source   = $('#entry-template').html();
+    var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
-    var html     = template(data);
-    
+    var html = template(data);
+
     $('body').append(html);
 
     $('li').click(function(){
         var selected_answer = $(this).attr('value');
-        var correct_answer  = String(data['answer']);
-        
+        var correct_answer = String(data['answer']);
+
         if(selected_answer == correct_answer){
             $(this).css('background-color', 'green');
             location.reload();
